c3dc0ddeffce7b47f6a3d6e14212bac4cf9b37fa
Mister-Corn <code@ronaldlibago.com>
2019-01-05 13:30:20 -0800

    Implement killPlayer, seerSight, and checkVictory methods

--- backend/src/controllers/Game.ts
+++ backend/src/controllers/Game.ts
@@ -17,22 +17,36 @@ class Player {
   name: string;
   role: "townsperson" | "werewolf" | "seer";
   alive: boolean;
+  seenBySeer: boolean;
   constructor(name: string) {
     this.name = name;
     this.role = "townsperson";
     this.alive = true;
+    this.seenBySeer = false;
   }
 }
 
-class WerewolfGame {
+export class WerewolfGame {
   players: Player[];
+  maxNumOfWerewolves: number;
+  maxNumOfSeers: number;
+  werewolves: Player[];
+  seers: Player[];
   constructor(names: string[]) {
     this.players = names.map(name => new Player(name));
+    this.werewolves = [];
+    this.seers = [];
+    this.maxNumOfWerewolves = 2;
+    this.maxNumOfSeers = 1;
+
+    this.determineRoles("werewolf", this.maxNumOfWerewolves, this.werewolves);
+    this.determineRoles("seer", this.maxNumOfSeers, this.seers);
   }
 
   determineRoles = (
     role: "townsperson" | "werewolf" | "seer",
-    numberOfPlayersToAssign: number
+    numberOfPlayersToAssign: number,
+    playerList: Player[]
   ): void => {
     /*
     Assigns a number of players to a role.
@@ -44,9 +58,35 @@ class WerewolfGame {
       const index: number = generateRandomNum(this.players.length - 1);
       if (this.players[index].role === "townsperson") {
         this.players[index].role = role;
+        playerList.push(this.players[index]);
         assignedPlayers += 1;
       }
     }
+  };
+
+  killPlayer = (index: number): Player[] => {
+    if (this.players[index].alive) this.players[index].alive = false;
+    return this.players;
+  };
+
+  seerSight = (): Player => {
+    for (const werewolf of this.werewolves) {
+      if (!werewolf.seenBySeer) {
+        werewolf.seenBySeer = true;
+        return werewolf;
+      }
+    }
+    return null;
+  };
+
+  checkVictory = (): "townsperson" | "werewolf" => {
+    const numOfWerewolves = this.werewolves.length;
+    const numOfPlayers = this.players.length;
+
+    if (numOfWerewolves >= numOfPlayers) return "werewolf";
+    if (numOfWerewolves === 0) return "townsperson";
+
+    return null;
   };
 }
 
---
+++ backend/src/controllers/GameLoop.ts
@@ -0,0 +1,5 @@
+import { WerewolfGame } from "./Game";
+
+function gameLoop(playersList: string[]): void {
+  const game = new WerewolfGame(playersList);
+}

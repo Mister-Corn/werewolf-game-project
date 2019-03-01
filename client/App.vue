1ee37f27af2bea69a63144ee5face4183f423700
Ronald Libago <code@ronaldlibago.com>
2019-03-01 14:57:47 -0800

    Set up main.html and main.js for Vue

---
+++ client/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>This is a Vue component and below is the current date:
+      <br>
+      {{date}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            date: new Date(),
+        };
+    }
+}
+</script>
+
+<style>
+p {
+    font-size: 2em;
+    text-align: center;
+}
+</style>
